a7eaff21635d4d90d697c13704040727c4a1e9ff
Crt Gregoric <crt.gregoric@gmail.com>
2025-04-26 21:49:25 +0200

    Added home row mods

--- keyboards/splitkb/aurora/sofle_v2/keymaps/crtgregoric/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/crtgregoric/keymap.c
@@ -46,13 +46,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+// Left-hand home row mods
+#define SFT_A LSFT_T(KC_A)
+#define CTL_S LCTL_T(KC_S)
+#define ALT_D LALT_T(KC_D)
+#define GUI_F LGUI_T(KC_F)
+
+// Right-hand home row mods
+#define GUI_J RGUI_T(KC_J)
+#define ALT_K LALT_T(KC_K)
+#define CTL_L RCTL_T(KC_L)
+#define SFT_SCLN RSFT_T(KC_SCLN)
+
 // --- Keymaps ---
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base Layer
     [0] = LAYOUT(
         KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
-        KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+        KC_CAPS,    SFT_A,      CTL_S,      ALT_D,      GUI_F,      KC_G,                                   KC_H,       GUI_J,      ALT_K,      CTL_L,      SFT_SCLN,   KC_QUOT,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       XXXXXXX,        XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
                                 KC_LCTL,    LALT_K,     LGUI_K,     KC_SPC,     MO(2),          KC_ENT,     RGUI_K,     MO(1),      RALT_K,     KC_RCTL
     ),

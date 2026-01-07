dd256e2dc71a7718862b45f67a24c06da26992d8
Crt Gregoric <crt.gregoric@gmail.com>
2026-01-07 16:50:16 +0100

    Updated keymap

--- keyboards/splitkb/aurora/sofle_v2/keymaps/crtgregoric/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/crtgregoric/keymap.c
@@ -57,7 +57,7 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base Layer
     [0] = LAYOUT(
-        KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
+        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
         KC_CAPS,    SFT_A,      CTL_S,      ALT_D,      GUI_F,      KC_G,                                   KC_H,       GUI_J,      ALT_K,      CTL_L,      SFT_SCLN,   KC_QUOT,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       XXXXXXX,        XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
@@ -65,17 +65,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     // Raise Layer
     [1] = LAYOUT(
-        KC_GRV,     _______,    _______,    _______,    _______,    _______,                                _______,    _______,    _______,    KC_MINS,    KC_EQL,     KC_DEL,
-        _______,    _______,    _______,    _______,    _______,    _______,                                _______,    _______,    KC_UP,      KC_LBRC,    KC_RBRC,    _______,
-        _______,    _______,    _______,    _______,    _______,    _______,                                _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    KC_ENT,
+        KC_ESC,     KC_BRMD,    KC_BRMU,    RM_VALD,    RM_VALU,    _______,                                _______,    _______,    _______,    _______,    KC_MINS,    KC_EQL,
+        _______,    _______,    _______,    _______,    _______,    _______,                                _______,    _______,    KC_UP,      _______,    KC_LBRC,    KC_RBRC,
+        _______,    _______,    _______,    _______,    _______,    _______,                                _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
                                 _______,    _______,    _______,    _______,    MO(3),          _______,    _______,    _______,    _______,    _______
     ),
     // Lower Layer
     [2] = LAYOUT(
         KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                                  KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-        KC_BRMD,    KC_BRMU,    KC_NO,      KC_NO,      RM_VALD,    RM_VALU,                                KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,
-        _______,    _______,    _______,    _______,    _______,    _______,                                _______,    _______,    _______,    _______,    _______,    KC_ENT,
+        _______,    _______,    _______,    _______,    _______,    _______,                                KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,
+        _______,    _______,    _______,    _______,    _______,    _______,                                _______,    _______,    _______,    _______,    _______,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
                                 _______,    _______,    _______,    _______,    _______,        _______,    _______,    MO(3),      _______,    _______
     ),
